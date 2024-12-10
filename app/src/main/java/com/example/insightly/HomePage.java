fc8a27f7996d4472eec493a7a12b2e4cd45cbf0b
Dhrubo <dhrubochando@gmail.com>
2024-12-11 01:40:02 +0600

    Insightly.First commit(5). Navigation drawer.

--- app/src/main/java/com/example/insightly/HomePage.java
+++ app/src/main/java/com/example/insightly/HomePage.java
@@ -5,43 +5,102 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.insightly.databinding.ActivityHomePageBinding;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomePage extends AppCompatActivity {
-    private TextView name, email;
-    private Button logOut;
-    private GoogleSignInOptions googleSignInOptions;
+    private ActivityHomePageBinding binding;
     private GoogleSignInClient googleSignInClient;
-    ActivityHomePageBinding binding;
-
-    @SuppressLint("ResourceType")
+    private DrawerLayout drawerLayout;
+    private ImageView profile, notifications;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHomePageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        profile = findViewById(R.id.iv_profile);
+        notifications = findViewById(R.id.iv_notifications);
+
+//        // Set up the toolbar
+//        setSupportActionBar(binding.toolbar);
+
+        // Initialize DrawerLayout
+        drawerLayout = binding.drawerLayout;
+
+//        // Set up ActionBarDrawerToggle
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this,
+//                drawerLayout,
+//                binding.toolbar,
+//                R.string.open_drawer,
+//                R.string.close_drawer
+//        );
+//        drawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
+
+        // Set up NavigationView
+        binding.navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if(id == R.id.nav_logout) {
+                    signOut();
+                }
+
+//                switch (id) {
+//                    case R.id.nav_home:
+//                        replaceFragment(new HomeFragment());
+//                        break;
+//                    case R.id.nav_profile:
+//                        replaceFragment(new SearchFragment());
+//                        break;
+//                    case R.id.nav_settings:
+//                        replaceFragment(new Option2Fragment());
+//                        break;
+//                    case R.id.nav_logout:
+//                        replaceFragment(new Option3Fragment());
+//                        break;
+//                    case R.id.nav_logout:
+//                        signOut();
+//                        break;
+//                }
+
+                // Close the drawer after selecting an item
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.openDrawer(GravityCompat.START); // Open drawer
+                } else {
+                    drawerLayout.closeDrawer(GravityCompat.START); // Close drawer if already open
+                }
+            }
+        });
+
+        // Default fragment
         replaceFragment(new HomeFragment());
 
+        // Set up BottomNavigationView
         binding.bottomNavigationMenu.setOnItemSelectedListener(menuItem -> {
             int itemId = menuItem.getItemId();
 
@@ -56,26 +115,6 @@ public class HomePage extends AppCompatActivity {
             }
             return true;
         });
-
-        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
-
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-        if(account != null) {
-            String personName = account.getDisplayName();
-            String emailId = account.getEmail();
-
-            name.setText(personName);
-            email.setText(emailId);
-        }
-
-        logOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOut();
-            }
-        });
-
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -83,17 +122,13 @@ public class HomePage extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
-
     }
 
     private void signOut() {
-        googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        googleSignInClient.signOut().addOnCompleteListener(task -> {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
